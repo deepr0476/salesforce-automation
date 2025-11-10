55efd16daf8c530d91424e5a03e488b04838d8ea
Ramandeep <deepr0476@gmail.com>
2025-11-10 12:25:30 +0530

    minor update

--- playwright.config.js
+++ playwright.config.js
@@ -1,18 +1,35 @@
-// playwright.config.js
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// 🔹 ES module me __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 0,
+  expect: { timeout: 10000 },
+  reporter: [['list'], ['html', { outputFolder: 'Reports/playwright-report' }]],
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        headless: false,
+        viewport: { width: 1920, height: 1080 },
+        ignoreHTTPSErrors: true,
+        screenshot: 'only-on-failure',
+        video: {
+          mode: 'retain-on-failure',
+          dir: path.join(__dirname, 'Reports/videos'),
+        },
+        trace: 'retain-on-failure',
+      },
     },
   ],
   use: {
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    actionTimeout: 0,
     baseURL: 'https://login.salesforce.com',
+    actionTimeout: 0,
   },
 });
